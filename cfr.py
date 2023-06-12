7e4e3dd0824fc77b1d1e7a8d668d2148eca9efa3
davpat108 <ddavidpatrikk@gmail.com>
2023-06-13 00:31:48 +0200

    CFR done, ready to implement the game

--- cfr.py
+++ cfr.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 class CFRNode:
-    def __init__(self, game, num_actions, player_id, parent=None, policy=None, cfr_strategy_func=None, action_choice_func=None):
+    def __init__(self, game, num_actions, player_id, parent=None, policy=None, cfr_strategy_func=None, action_choice_func=None, player_count = 6):
         self.game = game
         self.num_actions = num_actions
 
@@ -14,20 +14,15 @@ class CFRNode:
         self.cumulative_regrets = np.zeros(len(self.children))
         self.strategy = np.zeros(len(self.children))
         self.cumulative_strategy = np.zeros(len(self.children))
+        self.node_value = np.zeros(player_count) # For the current_player node_value[player_id] = reward
         
-        # Optionally initialize a policy
-        self.policy = np.ones(len(self.children)) / num_actions if policy is None else policy
-        
-        # Initialize strategy update and action choice functions
-        self.cfr_strategy_func = self.default_cfr_strategy if cfr_strategy_func is None else cfr_strategy_func
-        self.action_choice_func = self.default_action_choice if action_choice_func is None else action_choice_func
-
-    def default_cfr_strategy(self, cumulative_regrets):
-        # Implement the default strategy update logic here
-        pass
 
     def action_choice(self):
         # returns a child node, so node and option
+        pass
+
+    def expand(self):
+        # Account for player
         pass
 
     def is_terminal(self):
@@ -45,34 +40,45 @@ class CFRNode:
         node = self
         for iteration in range(max_iterations):
             # Traverse
-            node.update_strategy() 
             node, option = node.action_choice()
 
             # leaf node
             if node.children == []:
                 node.expand()
                 # terminal node, get rewards, calc regrets, backpropagate
-                if node.children == []:
+                if node.is_terminal():
                     reward = node.get_reward()
-                    node.backpropagate(reward) 
+                    node.backpropagate(reward) # backpropagate the reward and calculate regrets
+                    node.update_strategy()
                     node = self
         
-        return self.action_choice()
 
-
-        
     def update_regrets(self):
         # backprops, checks all the other choices it could have made from the parent and calcs reward
         # pay attention to player id
-        pass
+
+        player_id = self.player_id
+        # Calculate the actual rewards for each action
+        actual_rewards = [child.node_value[player_id] for child in self.children]
+        # Get the maximum possible reward
+        max_reward = max(actual_rewards)
+        # Update regrets
+        for a in range(len(self.children)):
+            self.cumulative_regrets[a] += max_reward - actual_rewards[a]
     
-    def get_reward(self):
-        # Implement the logic to get the reward here
-        pass
 
     def backpropagate(self, reward):
-        # Implement the logic to backpropagate the reward here
-        pass
+        if self.parent is None:
+            return
+
+        # Update the value of this node
+        self.node_value += reward
+
+        # Calculate regret for this node
+        self.update_regrets(reward)
+
+        # Recursively call backpropagate on parent node
+        self.parent.backpropagate(reward)
 
     def update_strategy(self):
         total_regret = np.sum(self.cumulative_regrets)
@@ -87,6 +93,3 @@ class CFRNode:
         # Update the cumulative strategy used for the average strategy output
         self.cumulative_strategy += self.strategy
     
-    def expand(self):
-        # Account for player
-        pass
