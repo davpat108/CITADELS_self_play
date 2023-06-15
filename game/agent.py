3c5eae3c7e0f0ff7fe7f54d9b6d6b7a036ab8da7
davpat108 <ddavidpatrikk@gmail.com>
2023-06-15 23:00:31 +0200

    agent started

--- game/agent.py
+++ game/agent.py
@@ -1,4 +1,5 @@
 from deck import Deck
+from option import option
 
 class Agent():
 
@@ -7,8 +8,23 @@ class Agent():
         self.hand = Deck(empty=True)
         self.role = None
         self.buildings = Deck(empty=True)
-        self.warrant = False
-        self.blackmail = False
+        self.warrant_fake = False
+        self.warrant_true = False
+        self.blackmail_fake = False
+        self.blackmail_true = False
         self.crown = False
         self.money = 2
         self.id = id
+
+    def choose_role_options(self, avaible_roles:list) -> list:
+        options = [option(choice=role, name="Role") for role in avaible_roles]
+        return options
+    
+    def gold_or_card(self) -> list:
+        options = [option(choice="gold", name="gold_or_card"), option(choice="gold", name="gold_or_card")]
+        return options
+    
+    def blackmail_response_options(self, ) -> list:
+        if self.blackmail_fake or self.blackmail_true:
+            return [option(choice="pay", name="blackmail_response"), option(choice="not_pay", name="blackmail_response")]
+        return None
--- game/option.py
+++ game/option.py
@@ -1,6 +1,8 @@
 class option():
     def __init__(self, **kwargs):
+        self.name = kwargs['name']
         self.attributes = kwargs
-    
-    def carry_out_action(self):
+
+
+    def carry_out_action(self, game):
         pass
